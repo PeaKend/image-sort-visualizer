766a665d800d5893ff7db9b4a7c480bd1a15f969
Facundo Atrio <peakend22@gmail.com>
2019-02-11 01:19:36 -0300

    removed fill

--- bubblesort.py
+++ bubblesort.py
@@ -78,7 +78,6 @@ def init():
     
             pygame.display.update()
             pygame.time.delay(100)
-            screen.fill((0, 0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
